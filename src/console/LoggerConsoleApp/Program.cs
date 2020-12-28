ad8a5e8aab9a6d212abba90087b4c5f401173dde
Ferdinand C <OksXen@users.noreply.github.com>
2020-12-28 07:55:59 -0500

    init commit

---
+++ src/console/LoggerConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
